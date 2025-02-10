7fac0e51c19815ea6d4a8ecb17a7526a009bd52e
johnsonavery <johnsonaveryal@gmail.com>
2025-02-09 19:24:44 -0500

    added code for add on total and meals total

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,52 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+        img: "/src/images/business-20031_1280.jpg",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "/src/images/speakers-4109274_1280.jpg",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "/src/images/public-speaking-3926344_1280.jpg",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "/src/images/whiteboard-2903269_1280.png",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+    {
+        img: "/src/images/signpost-235079_1280.jpg",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }
     },
     decrementAvQuantity: (state, action) => {
-     
+       const item = state[action.payload];
+       if(item && item.quantity > 0)
+       {
+           item.quantity--;
+       }
     },
   },
 });
